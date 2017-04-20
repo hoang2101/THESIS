87bb47328502bdd973eae7cf76e8e78940017080
thanhluand0147 <thanhluand@gmail.com>
2017-04-20 17:58:16 +0700

    index

--- resources/views/main/index.php
+++ resources/views/main/index.php
@@ -88,10 +88,8 @@
                 <li><a href="#aboutUs">About Us</a></li>
                 <li><a href="#whyUs">Why Us</a></li>
                 <li><a href="#pricing">Pricing</a></li>
-                <li><a href="#footer">Contact</a></li>
-                <li><a id="btn-login" class="btn" ng-click="showLogin($event)">Login</a></li>   
-                <li><a id="btn-reg" class="btn" ng-click="showRegister($event)">Register</a></li> 
-                <li><a href="#" data-toggle="modal" data-target="#login-modal">Login</a></li> 
+                <li><a href="#footer">Contact</a></li> 
+                <li><a href="#" data-toggle="modal" data-backdrop="static" data-target="#login-modal">Login</a></li> 
                 
 
 <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" style="display: none;">
@@ -110,45 +108,7 @@
         </div>
       </div>
       </div>
-                <li class="dropdown">
-                  <a href="#" class="dropdown-toggle" data-toggle="dropdown"><b>Login</b> <span class="caret"></span></a>
-                    <ul id="login-dp" class="dropdown-menu">
-                      <li>
-                         <div class="row">
-                            <div class="col-md-12">
-                              Login via
-                              <div class="social-buttons">
-                                <a href="#" class="btn btn-fb"><i class="fa fa-facebook"></i> Facebook</a>
-                                <a href="#" class="btn btn-tw"><i class="fa fa-twitter"></i> Twitter</a>
-                              </div>
-                                              or
-                               <form class="form" role="form" method="post" action="login" accept-charset="UTF-8" id="login-nav">
-                                  <div class="form-group">
-                                     <label class="sr-only" for="exampleInputEmail2">Email address</label>
-                                     <input type="email" class="form-control" id="exampleInputEmail2" placeholder="Email address" required>
-                                  </div>
-                                  <div class="form-group">
-                                     <label class="sr-only" for="exampleInputPassword2">Password</label>
-                                     <input type="password" class="form-control" id="exampleInputPassword2" placeholder="Password" required>
-                                                           <div class="help-block text-right"><a href="">Forget the password ?</a></div>
-                                  </div>
-                                  <div class="form-group">
-                                     <button type="submit" class="btn btn-primary btn-block">Sign in</button>
-                                  </div>
-                                  <div class="checkbox">
-                                     <label>
-                                     <input type="checkbox"> keep me logged-in
-                                     </label>
-                                  </div>
-                               </form>
-                            </div>
-                            <div class="bottom text-center">
-                              New here ? <a href="#"><b>Join Us</b></a>
-                            </div>
-                         </div>
-                      </li>
-                    </ul>
-                </li>
+                
               </ul>           
             </div><!--/.nav-collapse -->
           </div>     

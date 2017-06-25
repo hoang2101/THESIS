5f974d860da73384b5df821a757f6b5ed0fcc69f
thanhluand0147 <thanhluand@gmail.com>
2017-06-26 01:30:26 +0700

    fix

--- resources/views/main/report.blade.php
+++ resources/views/main/report.blade.php
@@ -210,7 +210,7 @@
                 <div class="col-md-12 col-sm-12 col-xs-12">
                 <div class="x_panel">
                   <div class="x_title">
-                    <h2>Danh sách Khách hàng<small></small></h2>
+                    <h2>Thống kê<small></small></h2>
                     <ul class="nav navbar-right panel_toolbox">
                      
                     </ul>
